90dbc183a4871241f0badd84ef96d1917eee3811
durgesh19952517 <65106278+durgesh19952517@users.noreply.github.com>
2022-05-19 18:52:40 +0000

    Add files via upload

---
+++ BUSINFO/Businfo_boardingpoint_traveldate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+
+
+namespace ADO_BusInfo
+{
+    class BusInfoProg
+    {
+        static void Main(string[] args)
+        {
+            BusInfoProg.Connection();
+            Console.ReadLine();
+        }
+        public static void Connection()
+        {
+            string cs = "Data source=Durgesh;Initial Catalog=League;Integrated security=True";
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "SELECT BusID,BoardingPoint FROM BusInfo WHERE TravelDate='2017-12-10' ";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            { 
+                Console.WriteLine("BUS ID = " + dr["BusID"] +"Boarding Point = " +dr["BoardingPoint"]);
+            }
+            conn.Close();
+            
+        }
+    }
+}
---
+++ BUSINFO/SQLQuerY_businfo.sql
@@ -0,0 +1,44 @@
+Create database League;
+
+--creating table Footballleague
+
+create table FootBallLeague
+(
+MatchId int Identity(1001,1) primary key,
+TeamName1 nvarchar(50) ,
+TeamName2 nvarchar(50) ,
+MatchStatus nvarchar(50) Not NULL,
+WinningTeam nvarchar(50),
+Points int NOT NULL
+);
+
+--inserting data into footballleague table
+
+INSERT INTO FootBallLeague(TeamName1,TeamName2,MatchStatus,WinningTeam,Points)
+VALUES
+('Italy','France','Win','France',4),
+('Brazil','Portugal','Draw',null,2),
+('England','Japan','Win','England',4),
+('Usa','Russia','Win','Russia',4),
+('Portugal','Italy','Draw',null,2),
+('Brazil','France','Win','Brazil',4),
+('England','Russia','Win','Russia',4),
+('Japan','USA','Draw',null,2);
+
+
+Select * from FootBallLeague;
+go
+Create proc InsertDetails
+@TeamName1 nvarchar(50),
+@TeamName2 nvarchar(50),
+@Status nvarchar(50),
+@WinningTeam nvarchar(50),
+@points int
+as
+begin
+insert into FootBallLeague
+(TeamName1,TeamName2,MatchStatus,WinningTeam,Points)
+values(@TeamName1,@TeamName2,@Status,@WinningTeam,@points)
+end
+go
+
